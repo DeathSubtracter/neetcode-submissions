30ca0542866fc19c5ba82f9334f8c3f42479f8e1
DeathSubtracter <116607995+DeathSubtracter@users.noreply.github.com>
2026-09-07 13:25:33 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_counts = dict()
+        for char in s:
+            s_counts[char] = s_counts.get(char, 0) + 1
+        
+        for char in t:
+            if char not in s_counts:
+                return False
+            
+            s_counts[char] -= 1
+            if s_counts[char] < 0:
+                return False
+        
+        return True
